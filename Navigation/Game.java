feb8d1822aa9c43130af39f5a853b7b31b534da0
Laila Mota <lailamota.238@gmail.com>
2024-12-15 20:07:04 -0300

    Initializes variables for the underwater navigation game

---
+++ Navigation/Game.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+import java.util.Scanner;
+public class UnderwaterNavigationGame {
+
+    static int submarineX, submarineY;
+    static int score = 0;
+    static boolean gameRunning = true;
+    static char [][] ocean;
+    static int oceanWidth = 20, oceanHeight = 10;
+}
