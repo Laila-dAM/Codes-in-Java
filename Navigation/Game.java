bccb8eef73e8c71946fc3e4c25f59dd91f01a383
Laila Mota <lailamota.238@gmail.com>
2024-12-15 20:11:18 -0300

    Initializes the scanner, ocean and random

--- Navigation/Game.java
+++ Navigation/Game.java
@@ -7,4 +7,10 @@ public class UnderwaterNavigationGame {
     static boolean gameRunning = true;
     static char [][] ocean;
     static int oceanWidth = 20, oceanHeight = 10;
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
+    ocean = new char[oceanHeight][ocean Width];
+    Random rand = new Random();
 }
