053ff6f4b73d08955013fb93eef41e4c74380b44
Laila Mota <lailamota.238@gmail.com>
2024-12-15 19:18:30 -0300

    Checks whether environmental parameters are outside ideal limits

--- Green/green.java
+++ Green/green.java
@@ -31,5 +31,15 @@ class Greenhouse {
         this.humidity = humidity;
         this.lightIntensity = lightIntensity;
     }
+    
+    public void checkEnvironment() {
+        if (temperature < 18 || temperature > 30) {
+            System.out.println("Warning: Temperature out of optimal range (18-30°C)!");
+        }
+        if (humidity < 40 || humidity > 80) {
+            System.out.println("Warning: Humidity out of optimal range (40-80%)!");
+        }
+    }
+
 
 
