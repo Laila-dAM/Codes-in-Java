7ff87d9d76bb0a6b20cfea3df81dae39c2b3027f
Laila Mota <lailamota.238@gmail.com>
2024-12-17 18:56:42 -0300

    adjSpd adjusts the speed between 100 and 600; simWthr generates random wind and turbulence

--- Airplane/air.java
+++ Airplane/air.java
@@ -12,3 +12,13 @@ void adjAlt(int adj){
     if (alt < 0) alt = 0;
     else if (alt > 10000) alt = 10000;
 }
+
+void adjSpd(int adj){
+    spd += adj;
+    if (spd < 100) spd = 100;
+    else if (spd > 600) spd = 600;
+}
+void simWthr(){
+    wnd = rand() % 21 - 10;
+    turb = rand() % 3;
+}
