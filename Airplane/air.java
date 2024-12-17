4853ae482a78154ee5a23c86741b7e5b332d1ffc
Laila Mota <lailamota.238@gmail.com>
2024-12-17 18:36:21 -0300

    Sets altitude and speed, and adjusts altitude between 0 and 10000

--- Airplane/air.java
+++ Airplane/air.java
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int altitude = 1000;
+int speed = 300;
+int windSpeed = 0;
+int turbulence = 0;
+
+void adjustAltitude(int adjustment) {
+    altitude += adjustment;
+if (altitude < 0){
+    altitude = 0;
+} else if (altitude > 10000) {
+    altitude = 10000;
+}
+}
