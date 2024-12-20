eaacf04cef0ba70a34cca6849e10120827a0aa29
Laila Mota <lailamota.238@gmail.com>
2024-12-20 15:08:32 -0300

    Adjust speed, simulate weather (wind and turbulence) and display flight status (altitude, speed, wind and turbulence)

--- Airplane/air.java
+++ Airplane/air.java
@@ -12,5 +12,27 @@ public static void adjAlt(int adj){
     if (alt < 0) alt = 0;
     else if (alt > 10000) alt = 10000;
 }
-    
+public static void adjSpd(int adj){
+    spd += adj;
+    if (spd < 100) spd = 100;
+    else if (spd > 600) spd = 600;
+}   
+public static void simWthr(){
+    Random rand = new Random();
+    wnd = rand.nextInt(21) - 10;
+    turb = rand.nextInt(3);
+}
+
+public static void showStart() {
+    System.out.println("Altitude: " + alt + " feet");
+    System.out.println("Speed: " + spd + " knots");
+    System.out.println("Wind Speed: " + wnd + " knots");
+
+    if (turb == 0) System.out.println("Turbulence: None");
+    else if (turb == 1) System.out.println("Turbulence: Light");
+    else System.out.println("Turbulence: Heavy");
+}
+    public static void main(String[] args){
+        
+    }
 }
