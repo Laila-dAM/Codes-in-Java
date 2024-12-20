1ec054dba2a8dee1f572a9a36e44dae1f07916e7
Laila Mota <lailamota.238@gmail.com>
2024-12-20 14:57:22 -0300

    Sets variables to simulate flight and adjusts altitude within limits

--- Airplane/air.java
+++ Airplane/air.java
@@ -1,24 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+import java.util.Random;
+import java.util.Scanner;
 
-int alt = 1000;
-int spd = 300;
-int wnd = 0;
-int turb = 0;
+public class air {
+    static int alt = 1000;
+    static int spd = 300;
+    static int wnd = 0;
+    static int turb = 0;
 
-void adjAlt(int adj){
+public static void adjAlt(int adj){
     alt += adj;
     if (alt < 0) alt = 0;
     else if (alt > 10000) alt = 10000;
 }
-
-void adjSpd(int adj){
-    spd += adj;
-    if (spd < 100) spd = 100;
-    else if (spd > 600) spd = 600;
-}
-void simWthr(){
-    wnd = rand() % 21 - 10;
-    turb = rand() % 3;
+    
 }
