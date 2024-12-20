16f3bd87a19072985df2c21ff844a930287494fc
Laila Mota <lailamota.238@gmail.com>
2024-12-20 13:24:41 -0300

    Sets variables for the game, such as screen size, character position, score and elements, and initializes the timer

--- Hungry Danet/Game.java
+++ Hungry Danet/Game.java
@@ -5,6 +5,21 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Game extends JPanel implements ActionListener {
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
+    private final int CHARACTER_SIZE = 40;
+    private final int TIMER_DELAY = 50;
 
+    private int characterX;
+    private int characterY;
+    private int score;
+    private boolean gameOver;
 
+    private ArrayList<Element> elements;
+    private Random random = new Random();
+
+    private Timer timer;
+    public Game() {
+        
+    }
 }
